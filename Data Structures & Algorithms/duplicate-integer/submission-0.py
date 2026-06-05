9d5d94eb009933ce53ccde1691a2f7401899ad97
DIPANKAR BASAK <dbasak2013@gmail.com>
2026-06-05 13:57:32 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums_dict = {}
+        for n in nums:
+            if n in nums_dict:
+                return True
+            nums_dict[n] = n
+        return False
+        
